f07a9ca0f9bd9bdb7966045f32b49b86869d7b94
jpoly <jpoly@umich.edu>
2023-07-23 09:49:23 -0400

    #6. Submit, delete, and visibility menu for LMs

---
+++ src/components/lm/LmVisibilityDropdown.tsx
@@ -0,0 +1,47 @@
+import { LmDropdownItem } from ".";
+
+import { CourseraPlaybackLm, Lm } from "../../types";
+import { makePutReq } from "../../utils";
+
+interface Props {
+  lmArray: Lm[];
+  setLmArray: React.Dispatch<React.SetStateAction<Lm[]>>;
+  lmIndex: number;
+}
+
+const LmVisibilityDropdown = ({ lmArray, setLmArray, lmIndex }: Props) => {
+  const handleChange = () => {
+    if (lmArray.length > 0) {
+      const e = document.getElementById("lmSelectMenu") as HTMLSelectElement;
+      const newLmArray: CourseraPlaybackLm[] = JSON.parse(JSON.stringify(lmArray));
+      newLmArray[lmIndex].visibility = e.value;
+      setLmArray(newLmArray);
+
+      // Push changes to server.
+      const payload = newLmArray[lmIndex];
+      console.log("payload:", payload);
+      makePutReq("/lms", payload);
+    }
+  };
+
+  return (
+    <div id="lmDropdownContainer">
+      <select
+        name=""
+        id="lmSelectMenu"
+        onChange={handleChange}
+        value={lmArray.length > 0 ? lmArray[lmIndex].visibility : ""}
+      >
+        {lmArray.length > 0 && (
+          <>
+            <LmDropdownItem value={"development"} />;
+            <LmDropdownItem value={"review"} />;
+            <LmDropdownItem value={"production"} />;
+          </>
+        )}
+      </select>
+    </div>
+  );
+};
+
+export { LmVisibilityDropdown };
