949b0151395ef1091da8ba9e9074dfecf6cfa98b
jpoly <jpoly@umich.edu>
2023-08-02 09:20:36 -0400

    move to prod

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-import { AUTH0_CLIENTID, AUTH0_DOMAIN } from "./utils/constants.ts";
+import { AUTH0_CLIENT_ID, AUTH0_DOMAIN } from "./utils/constants.ts";
 import App from "./App.tsx";
 import "./styles/index.css";
 
@@ -11,7 +11,7 @@ console.log(window.location.origin);
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Auth0Provider
     domain={AUTH0_DOMAIN}
-    clientId={AUTH0_CLIENTID}
+    clientId={AUTH0_CLIENT_ID}
     authorizationParams={{
       redirect_uri: window.location.origin,
     }}
